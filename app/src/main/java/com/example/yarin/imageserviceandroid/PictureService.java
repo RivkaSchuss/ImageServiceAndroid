d3d229dc1145a61f92a5a51cefb54f6f2d85fedb
Yarin <yarin.shaked7@gmail.com>
2018-06-12 17:15:13 +0300

    checking wifi

--- app/src/main/java/com/example/yarin/imageserviceandroid/PictureService.java
+++ app/src/main/java/com/example/yarin/imageserviceandroid/PictureService.java
@@ -1,12 +1,25 @@
 package com.example.yarin.imageserviceandroid;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
@@ -15,6 +28,8 @@ import java.net.Socket;
 public class PictureService extends Service{
 
     private Socket socket;
+    private BroadcastReceiver receiver;
+    private OutputStream outputStream;
 
     @Nullable
     @Override
@@ -30,7 +45,7 @@ public class PictureService extends Service{
             InetAddress serverAddr = InetAddress.getByName("127.0.0.1");
             socket = new Socket(serverAddr, 8000);
             try{
-                OutputStream output = socket.getOutputStream();
+                outputStream = socket.getOutputStream();
                 //FileInputStream fis = new FileInputStream(pic);
                 /////////
                 //output.write(imgbyte);
@@ -46,6 +61,9 @@ public class PictureService extends Service{
 
     public int onStartCommand(Intent intent, int flag, int startId) {
         Toast.makeText(this, "Service starting...", Toast.LENGTH_SHORT).show();
+
+        establishWifi();
+
         return START_STICKY;
     }
 
@@ -59,5 +77,62 @@ public class PictureService extends Service{
         } catch (IOException e) {
             Log.e("TCP", "S: Error:", e);
         }
+    }
+
+    public void establishWifi() {
+        final IntentFilter theFilter = new IntentFilter();
+        theFilter.addAction("android.net.wifi.supplicant.CONNECTION_CHANGE");
+        theFilter.addAction("android.net.wifi.STATE_CHANGE");
+        this.receiver = new BroadcastReceiver() {
+
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                WifiManager wifiManager = (WifiManager) context
+                        .getSystemService(Context.WIFI_SERVICE);
+                NetworkInfo networkInfo = intent.getParcelableExtra(wifiManager.EXTRA_NETWORK_INFO);
+                if (networkInfo != null) {
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        //get the different network states
+                        if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                            startTransfer(); // Starting the Transfer
+                        }
+                    }
+                }
+            }
+        };
+        // Registers the receiver so that your service will listen for
+        // broadcasts
+        this.registerReceiver(this.receiver, theFilter);
+    }
+
+    public void startTransfer() {
+        // Getting the Camera Folder
+        File dcim = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        if (dcim == null) {
+            return;
+        }
+        File[] pics = dcim.listFiles();
+        int count =0;
+        if (pics != null) {
+            for (File pic : pics) {
+                try {
+                    FileInputStream fis = null;
+                    fis = new FileInputStream(pic);
+                    Bitmap bm = BitmapFactory.decodeStream(fis);
+                    byte[] imgbyte = getBytesFromBitmap(bm);
+                    outputStream.write(imgbyte);
+                    outputStream.flush();
+                    count++;
+                } catch(Exception e) {
+                    Log.e("TCP", "S: Error:", e);
+                }
+            }
+        }
+    }
+
+    public byte[] getBytesFromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 70, stream);
+        return stream.toByteArray();
     }
 }
